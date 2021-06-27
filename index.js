9a09bd0e8ec3e808ce36f89b57617286acd0d89d
Trâm Hà <hatranbichtram94@gmail.com>
2021-06-27 17:20:41 +0700

    'console_check'

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 function master() {
 	console.log('master');
 }
+function test () {
+    console.log('testBranch');
+}
+function dev () {
+    console.log('devBranch');
+}
+function CheckTask() {
+    console.log('testTask');
+}
